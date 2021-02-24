b046f5cd6cdf6809d752fa01b2aca0c6fc597b1a
vinodsupnekar <vinod.supnekar@gmail.com>
2021-02-24 10:21:17 +0530

    Integration test added

--- Tests/FeedStoreIntegrationTests.swift
+++ Tests/FeedStoreIntegrationTests.swift
@@ -27,9 +27,9 @@ class FeedStoreIntegrationTests: XCTestCase {
 	}
 	
 	func test_retrieve_deliversEmptyOnEmptyCache() {
-//		        let sut = makeSUT()
-//
-//		        expect(sut, toRetrieve: .empty)
+		        let sut = makeSUT()
+
+		        expect(sut, toRetrieve: .empty)
 	}
 	
 	func test_retrieve_deliversFeedInsertedOnAnotherInstance() {
